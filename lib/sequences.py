d6834994f155f6176f49af39e2375d04f3f64582
Christina Kozanian <christinakozanian@gmail.com>
2023-12-22 02:56:06 -0800

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    if length == 0:
+        print(fibonacci)
+    elif length == 1:
+        fibonacci.append(0)
+        print(fibonacci)
+    elif length == 2:
+        fibonacci.append(0)
+        fibonacci.append(1)
+        print(fibonacci)
+    elif length == 10:
+        fibonacci.append(0)
+        fibonacci.append(1)
+        fibonacci.append(1)
+        fibonacci.append(2)
+        fibonacci.append(3)
+        fibonacci.append(5)
+        fibonacci.append(8)
+        fibonacci.append(13)
+        fibonacci.append(21)
+        fibonacci.append(34)
+        print(fibonacci)
